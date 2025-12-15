439a9164b0462030a2ab3fd388e2556279febed7
khgty1226 <khgty1226@naver.com>
2025-12-15 19:42:35 +0900

    gh-pages set

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import Coin from "./routes/Coin";
 
 function Router(){
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/crypto-tracker">
             <Switch>
                 <Route path="/:coinId">
                     <Coin></Coin>
